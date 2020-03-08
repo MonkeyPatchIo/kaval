d5e43b4770e5ca844dd6f3a5f95c747de9812f49
igor <ilaborie@gmail.com>
2020-03-08 13:16:06 +0100

    :bookmark: release new version:  '0.1.0'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,20 @@
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform") version Libs.kotlinVersion
     id("org.jetbrains.dokka") version Libs.dokkaVersion
-    id("maven-publish")
     id("java-library")
+    id("base")
 
     id("com.github.ben-manes.versions") version Libs.versions
     id("org.jmailen.kotlinter") version Libs.kotlinter
     id("com.adarshr.test-logger") version Libs.testLogger
     id("net.researchgate.release") version Libs.release
-    id("com.jfrog.bintray") version Libs.bintray
 }
 
 repositories {
     jcenter()
     mavenCentral()
-}
-
-publishing {
-    publications.withType<MavenPublication>() {
-        //artifact(tasks["javadocJar"])
-        pom {
-            group = "io.monkeypatch.kaval"
-
-            name.set("Kaval")
-            description.set("A POJO validation DSL in Kotlin")
-            inceptionYear.set("2020")
-
-            licenses {
-                license {
-                    name.set("The Apache Software License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                }
-            }
-            developers {
-                developer {
-                    name.set("Igor Laborie")
-                    email.set("igor@monkeypatch.io")
-                    organization.set("MonkeyPatch")
-                    organizationUrl.set("https://monkeypatch.io/")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://github.com/MonkeyPatchIo/kaval")
-                developerConnection.set("scm:git:ssh://github.com:MonkeyPatchIo/kaval.git")
-                url.set("http://github.com/MonkeyPatchIo/kaval/tree/master")
-            }
-        }
-    }
 }
 
 allprojects {
@@ -72,34 +37,4 @@ release {
     preTagCommitMessage = ":bookmark: release new version: "
     tagCommitMessage = ":bookmark: release new version: "
     newVersionCommitMessage = ":bookmark: prepare new version: "
-}
-
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_KEY")
-    setPublications("metadata", "jvm", "js")
-    publish = true //[Default: false] Whether version should be auto published after an upload
-    pkg.apply {
-        repo = "kaval"
-        name = "kaval"
-        userOrg = "monkeypatchio"
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/MonkeyPatchIo/kaval"
-        websiteUrl = "https://github.com/MonkeyPatchIo/kaval"
-        issueTrackerUrl = "https://github.com/MonkeyPatchIo/kaval/issues"
-
-        githubRepo = "monkeypatchio/kaval"
-        version.apply {
-            name = "${project.version}"
-            desc = "A POJO validation DSL in Kotlin"
-//            released = "${java.util.Date()}"
-            gpg.sign = true //Determines whether to GPG sign the files. The default is false
-//            mavenCentralSync.apply {
-//                sync = true //[Default: true] Determines whether to sync the version to Maven Central.
-//                user = "token" //OSS user token: mandatory
-//                password = "pass" //OSS user password: mandatory
-//                close = "1" //Optional property. By default the staging repository is closed and artifacts are released to Maven Central. You can optionally turn this behaviour off (by puting 0 as value) and release the version manually.
-//            }
-        }
-    }
 }
--- kaval-reflect/build.gradle.kts
+++ kaval-reflect/build.gradle.kts
@@ -3,8 +3,9 @@ plugins {
     id("kotlin-multiplatform")
     id("maven-publish")
     id("java-library")
-
+    id("signing")
     id("org.jetbrains.dokka")
+
     id("org.jmailen.kotlinter")
     id("com.adarshr.test-logger")
 }
@@ -14,7 +15,17 @@ repositories {
 }
 
 kotlin {
-    jvm()
+    metadata {
+        mavenPublication {
+            artifactId = "${project.name}-common"
+        }
+    }
+    jvm {
+        mavenPublication {
+            // make a name of jvm artifact backward-compatible, default "-jvm"
+            artifactId = project.name
+        }
+    }
     // js disable: This reflection API is not supported yet in JavaScript
 
     // HOWTO build with all available (compatible) platform ?
@@ -66,3 +77,61 @@ tasks {
         useJUnitPlatform()
     }
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "Maven Central"
+            url = uri(
+                if (version.toString().endsWith("SNAPSHOT")) "https://oss.sonatype.org/content/repositories/snapshots"
+                else "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            )
+            credentials {
+                username = System.getenv("NEXUS_USERNAME")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("reflect") {
+            from(components["java"])
+        }
+    }
+
+    publications.withType<MavenPublication>() {
+        //artifact(tasks["javadocJar"])
+        pom {
+            group = "io.monkeypatch.kaval"
+
+            name.set("Kaval")
+            description.set("A POJO validation DSL in Kotlin")
+            inceptionYear.set("2020")
+
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    name.set("Igor Laborie")
+                    email.set("igor@monkeypatch.io")
+                    organization.set("MonkeyPatch")
+                    organizationUrl.set("https://monkeypatch.io/")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/MonkeyPatchIo/kaval")
+                developerConnection.set("scm:git:ssh://github.com:MonkeyPatchIo/kaval.git")
+                url.set("http://github.com/MonkeyPatchIo/kaval/tree/master")
+            }
+        }
+    }
+}
+
+signing {
+    sign(configurations.archives.get())
+}
+
